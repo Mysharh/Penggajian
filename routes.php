bcdbfd2f843be15318ee967d7b93b4fd37dbd2b8
mysharoh <mysharh.st@gmail.com>
2024-05-20 14:42:54 +0800

    Initial commit

---
+++ routes.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = "";
+}
+
+switch ($page) {
+    case "";
+    case "dashboard";
+        include "pages/dashboard.php";
+        break;
+    case "bagian";
+        include "pages/bagian.php";
+        break;
+    case "bagiantambah";
+        include "pages/bagiantambah.php";
+        break;
+    case "bagianhapus":
+        include "pages/bagianhapus.php";
+        break;       
+    default;
+        include "pages/404.php";
+}
